6666a1d6d4f2cf8de05dbf4dbdbc7c43b107da36
Giovanna <112666597+AC-Giovanna@users.noreply.github.com>
2023-03-22 20:39:15 -0300

    Criação do Index.ejs

---
+++ index.js
@@ -0,0 +1,28 @@
+//Importações
+const express = require('express');
+const axios = require('axios');
+
+//Tornando o Express executável
+const app = express();
+
+//Tornando possível ler JSON e formulários
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//Configuração do EJS
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+//Rotas de acesso às páginas ejs
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+//ROTAS INÍCIO
+
+//ROTAS FIM
+
+//Servidor
+app.listen(3001, () => {
+    console.log('Servicor rodando em http://localhost:3001')
+});

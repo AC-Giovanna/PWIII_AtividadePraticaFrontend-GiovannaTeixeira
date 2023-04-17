5e4bbba9e8bbaf548edd114c313533344d2cc705
Giovanna <112666597+AC-Giovanna@users.noreply.github.com>
2023-04-17 01:27:15 -0300

    Cadastro e listagem de Jogadores e avatares

--- index.js
+++ index.js
@@ -44,26 +44,140 @@ app.get('/ListarTarefas', (req, res) => {
 //Edição
 app.get('/EditarTarefa/:id_tarefa', (req, res) => {
     
-    let {id_tarefa} = req.params;
+    let { id_tarefa } = req.params;
+
     urlListarTarefaPK = `http://localhost:3000/ListarTarefaPK/${id_tarefa}`;
 
     axios.get(urlListarTarefaPK)
-    .then((response) => {
-        let tarefa = response.data;
-        // console.log(tarefa.data);
-        res.render('tarefas/EditarTarefa', {tarefa})
-    })
+        .then((response) => {
+
+            let tarefa = response.data;
+            console.log(tarefa);
+            res.render('tarefas/EditarTarefa', {tarefa})
+        })
 });
 app.post('/EditarTarefa', (req, res) => {
-    let {id_tarefa} = req.params;
-    const urlEditarTarefa = `http://localhost:3000/AlterarTarefa/${id_tarefa}`;
+
+    console.log(req.body);
+
+    const urlEditarTarefa = `http://localhost:3000/AlterarTarefa`;
 
     axios.put(urlEditarTarefa, req.body)
+        .then((response) => {
+
+            const urlListarTarefas = 'http://localhost:3000/ListarTarefas';
+            axios.get(urlListarTarefas)
+                .then((response) => {
+                    console.log(response.data);
+                    let tarefas = response.data;
+                    res.render('tarefas/ListarTarefas', {tarefas});
+                });
+        });
+});
+
+//Exclusão
+app.get('/ExcluirTarefa/:id_tarefa', (req, res) => {
+    let {id_tarefa} = req.params;
+    const urlExcluirTarefa = `http://localhost:3000/DeletarTarefa/${id_tarefa}`;
+
+    axios.delete(urlExcluirTarefa)
     .then((response) => {
-        //res.redirect('/ListarTarefas');
-        console.log(response); 
-    })
+        const urlListarTarefas = 'http://localhost:3000/ListarTarefas';
+
+        axios.get(urlListarTarefas)
+        .then((response) => {
+            let tarefas = response.data;
+            res.render('tarefas/ListarTarefas', {tarefas});
+        });
+    });
+});
+
+//JOGADORES
+
+//Cadastro
+app.get('/CadastrarJogador', (req, res) => {
+    res.render('jogadores/CadastrarJogadores');
+});
+
+//Listagem
+app.get('/ListarJogadores', (req, res) => {
+    // Requisição para o backend pelo axios
+    const urlListarJogadores = 'http://localhost:3000/ListarJogadores';
+
+    axios.get(urlListarJogadores)
+    .then((response) => {
+        console.log(response.data);
+
+        let jogadores = response.data;
+        res.render('jogadores/ListarJogadores', {jogadores});
+    });
+});
+
+//Edição
+app.get('/EditarJogador/:id_jogador', (req, res) => {
+
+    let {id_jogador} = req.params;
+
+    // const urlSelecionarJogador
 })
+
+//Exclusão
+
+//AVATARES
+
+//Cadastro
+app.get('/CadastrarAvatar', (req, res) => {
+    res.render('avatares/CadastrarAvatares');
+});
+
+//Listagem
+app.get('/ListarAvatares', (req, res) => {
+    // Requisição para o backend pelo axios
+    const urlListarAvatares = 'http://localhost:3000/ListarAvatar';
+
+    axios.get(urlListarAvatares)
+    .then((response) => {
+        console.log(response.data);
+
+        let avatares = response.data;
+        res.render('avatares/ListarAvatares', {avatares});
+    });
+});
+
+//Edição
+app.get('/EditarAvatares/:id_avatar', (req, res) => {
+
+    let {id_avatar} = req.params;
+
+    const urlListarAvatarPK = `http://localhost:3000/ListarAvatarPK/${id_avatar}`;
+
+    axios.get(urlListarAvatarPK)
+        .then((response) => {
+            let avatar = response.data;
+            console.log(avatar);
+            res.render('avatares/EditarAvatares.ejs', {avatar});
+        });
+});
+app.post('/EditarAvatares', (req, res) => {
+
+    console.log(req.body);
+
+    const urlAlterarAvatar = `http://localhost:3000/AlterarAvatar`;
+
+    axios.put(urlAlterarAvatar, req.body)
+        .then((response) => {
+
+            const urlListarAvatar = `http://localhost:3000/ListarAvatar`;
+            axios.get(urlListarAvatar)
+                .then((response) => {
+                    console.log(response.data);
+                    let avatares = response.data;
+                    res.render('avatares/ListarAvatares', {avatares})
+                })
+        })
+})
+
+//Exclusão
 
 //ROTAS - FIM
 
